0810bf4abfb23654672e19653f3c7834e77e7bf9
Rivangek <rivangek@proton.me>
2024-12-17 17:01:39 -0600

    Add more token support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 mod tokenizer;
 
+use std::{env, fs};
+
 use tokenizer::Tokenizer;
 
 fn main() {
-    let mut tokenizer = Tokenizer::new("local object12 = 1444".to_string());
+    let mut current_path = env::current_dir().unwrap();
+    current_path.push("isolated.txt");
+
+    let content_result = fs::read_to_string(current_path);
+    let Ok(content_string) = content_result else {
+        return;
+    };
+
+    let mut tokenizer = Tokenizer::new(content_string);
     let tokens = tokenizer.capture();
 
     println!("{:?}", tokens);
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,6 +1,8 @@
 #[derive(Debug)]
-enum KeywordType {
-    Local
+pub enum KeywordType {
+    Local,
+    Function,
+    End
 }
 
 /// Type for proto-tokens, basically tokens whose type is still capturing and need a type of processor.
@@ -8,18 +10,22 @@ enum KeywordType {
 enum TokenProcessor {
     Malformed,
     Number,
-    Text,
-    Separator,
+    Identifier,
+    String,
+    Break, /// Break processor that literaly means skip.
     Operator
 }
 
 #[derive(Debug)]
 pub enum Token {
-    Quote,
     Identifier(String),
+    StringLiteral(String),
     NumberLiteral(i32),
     Keyword(KeywordType),
     Assign,
+    LeftParenthesis,
+    RightParenthesis,
+    UnexpectedSymbol(String)
 }
 
 pub struct Tokenizer {
@@ -30,6 +36,10 @@ pub struct Tokenizer {
 
     last_processor: TokenProcessor,
     current_processor: TokenProcessor,
+}
+
+fn is_string_token(c: &char) -> bool {
+    (c == &'"') | (c == &'\'')
 }
 
 impl Tokenizer {
@@ -53,6 +63,11 @@ impl Tokenizer {
             let captured_token = self.capture_token();
 
             if let Some(token) = captured_token {
+                if let Token::UnexpectedSymbol(symbol) = &token {
+                    //This isn't tokenizer business
+                    //panic!("Unexpected symbol \"{}\" at {}th index", symbol, self.current);
+                }
+
                 tokens.push(token);
             }
         }
@@ -63,6 +78,22 @@ impl Tokenizer {
     fn change_processor(&mut self, new: TokenProcessor) {
         self.last_processor = self.current_processor;
         self.current_processor = new;
+    }
+
+    fn is_processing_identifier(&self) -> bool {
+        if let TokenProcessor::Identifier = self.current_processor {
+            return true;
+        }
+
+        false
+    }
+
+    fn is_processing_string(&self) -> bool {
+        if let TokenProcessor::String = self.current_processor {
+            return true;
+        }
+
+        false
     }
 
     fn capture_token(&mut self) -> Option<Token> {
@@ -81,26 +112,34 @@ impl Tokenizer {
 
             // Choose a token processor
 
+            // Identifier
             if current_character.is_alphabetic() {
-                self.change_processor(TokenProcessor::Text);
+                if !self.is_processing_string() {
+                    self.change_processor(TokenProcessor::Identifier);
+                }
             }
 
-            if current_character.is_whitespace() {
-                self.change_processor(TokenProcessor::Separator);
+            // Break
+            if current_character.is_whitespace() && !self.is_processing_string() {
+                self.change_processor(TokenProcessor::Break);
             }
 
+            // String & operator
             if current_character.is_ascii_punctuation() {
-                self.change_processor(TokenProcessor::Operator);
+                if is_string_token(&current_character) {
+                    self.change_processor(TokenProcessor::String);
+                } else { // Operator case
+
+                    // If it isn't processing string proceed. (Prevents symbols in string to be considered as tokens)
+                    if !self.is_processing_string() {
+                        self.change_processor(TokenProcessor::Operator);
+                    }
+
+                }
             }
 
             if current_character.is_numeric() {
-                let processing_text = if let TokenProcessor::Text = self.current_processor {
-                    true
-                } else {
-                    false
-                };
-
-                if !processing_text {
+                if !self.is_processing_string() && !self.is_processing_identifier() {
                     self.change_processor(TokenProcessor::Number);
                 }
             }
@@ -108,26 +147,40 @@ impl Tokenizer {
             // Match the current processor to implement its logic
 
             match &self.current_processor {
-                TokenProcessor::Separator => {
+                TokenProcessor::Break => {
                     break;
                 }
 
                 TokenProcessor::Operator => {
                     token = match current_character {
                         '=' => Some(Token::Assign),
-                        _ => None
+                        '(' => Some(Token::LeftParenthesis),
+                        ')' => Some(Token::RightParenthesis),
+                        _ => Some(Token::UnexpectedSymbol(current_character.to_string()))
                     };
 
-                    break;
+                    break
                 }
 
-                TokenProcessor::Text => {
+                TokenProcessor::String => {
+                    // Same logic as identifier but includes double quotes 
+                    // and doesn't match for keywords.
+                    if !is_string_token(&current_character) {
+                        formed_string.push(current_character);
+                    }
+
+                    token = Some(Token::StringLiteral(formed_string.clone()))
+                }
+
+                TokenProcessor::Identifier => {
                     // This won't ignore numbers if the first character starts as an alphabetic.
                     // Only will stop when a separator is found using the next character.
                     formed_string.push(current_character);
 
                     token = match formed_string.as_str() {
                         "local" => Some(Token::Keyword(KeywordType::Local)),
+                        "function" => Some(Token::Keyword(KeywordType::Function)),
+                        "end" => Some(Token::Keyword(KeywordType::End)),
                         _ => Some(Token::Identifier(formed_string.clone()))
                     };
                 }
@@ -144,7 +197,19 @@ impl Tokenizer {
             // so the current token gets completed. Applies to all types of tokens.
 
             if let Some(character) = next_character {
-                if character.is_whitespace() {
+                if !self.is_processing_string() {
+                    if character.is_whitespace() {
+                        break;
+                    }
+    
+                    if character.is_ascii_punctuation() {
+                        break;
+                    }
+                }
+
+                // Allow string literal to form and skip the next character (which is the end of string literal).
+                if is_string_token(character) {
+                    self.current += 1; // Skip next character which is the next " that can start a new literal forming.
                     break;
                 }
             }
